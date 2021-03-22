affbc0eb260748ef8b36d62e8662d9f82ce187e0
Rob Clark <clarkrob13@gmail.com>
2021-03-22 14:52:23 -0400

    Initial

---
+++ VoterAnalysis/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoterAnalysis.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+                .HasData(
+                new IdentityRole
+                {
+                    Name = "CampaignManager",
+                    NormalizedName = "CAMPAIGNMANAGER"
+                },
+
+                new IdentityRole
+                {
+                    Name = "Staff",
+                    NormalizedName = "STAFF"
+                },
+
+                new IdentityRole
+                {
+                    Name = "Volunteer",
+                    NormalizedName = "VOLUNTEER"
+                }
+                );
+        }
+    }
+}
